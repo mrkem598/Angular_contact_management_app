89cbc8ddccfa7c6ac3db86546c42a3b63ffd7229
Moh Kemal <mohakemal9@gmail.com>
2017-08-13 15:21:47 -0400

    data added to the mlab with http post method using postman

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,43 @@
 const express = require('express');
 const app = express();
-
 const MongoClient = require('mongodb').MongoClient;
+//use body parser to parse data in to the body of the request
+const bodyParser = require('body-parser');
+const path = require('path');
 
 require('dotenv').config();
+app.use(bodyParser.json());
 
 let database;
+//prepend api to follow the api convention
+app.get('/api/contacts', (req, res) => {
+  const contactsCollection = database.collection('contacts');
 
+  contactsCollection.find({}).toArray((err, docs) => {
+    return res.json(docs);
+  });
+});
+app.post('/api/contacts', (req, res) => {
+const user = req.body;
+
+const contactsCollection = database.collection('contacts');
+
+contactsCollection.insertOne(user, (err, r) => {
+  if (err) {
+    // if its error send status code 500 which is a server code error
+    return res.status(500).json({ error: 'Error inseting new record.'});
+
+  }
+  const newRecord = r.ops[0];
+// status 201 means created!
+  return res.status(201).json(newRecord);
+});
+});
 MongoClient.connect(process.env.DB_CONN, (err, db) => {
   console.log('connected to mongodb...');
 
   app.listen(3000, () => {
     database = db;
     console.log('listening on port 3000..');
-  });
-});
-
-app.get('/contacts', (req, res) => {
-  const contactsCollection = database.collection('contacts');
-
-  contactsCollection.find({}).toArray((err, docs) => {
-    return res.json(docs);
   });
 });
