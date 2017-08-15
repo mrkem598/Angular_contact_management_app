c250adf374791afd7dc5bbd2b24ea9d46dd446b7
Moh Kemal <mohakemal9@gmail.com>
2017-08-15 16:08:03 -0400

    ngForm add-contact modified

--- src/client/app/add-contact/add-contact.component.spec.ts
+++ src/client/app/add-contact/add-contact.component.spec.ts
@@ -19,7 +19,7 @@ describe('AddContactComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
--- src/server/data/seed-db.js
+++ src/server/data/seed-db.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
-const users = require('./users');
-const contacts = require('./contacts');
-
 const MongoClient = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
 
+const users = require('./users');
+const contacts = require('./contacts');
+
+
 function seedCollection(collectionName, initialRecords) {
+
   MongoClient.connect(process.env.DB_CONN, (err, db) => {
     console.log('connected to mongodb...');
 
@@ -20,14 +22,18 @@ function seedCollection(collectionName, initialRecords) {
       }
     });
 
+    console.log('inserting records...');
+
     collection.insertMany(initialRecords, (err, result) => {
       console.log(`${result.insertedCount} records inserted.`);
       console.log('closing connection...');
       db.close();
       console.log('done.');
     });
+
   });
 }
+
 
 seedCollection('users', users);
 seedCollection('contacts', contacts);
