0f5c097b2182af05847c95a2015360d97fa74986
Moh Kemal <mohakemal9@gmail.com>
2017-08-16 08:43:19 -0400

    express app is created

--- src/server/create-express-app.js
+++ src/server/create-express-app.js
@@ -6,9 +6,11 @@ const apiRouter = require('./api-router');
 function createExpressApp(database) {
   const app = express();
 
+//use the static midleware in the body parser midle ware
   app.use(express.static(path.join(_dirname, 'public')));
   app.use('/profiles', express.static(path.join(_dirname)));
   app.use(bodyParser.json());
+  // Tell the expres to use the api router which taken the database connection
   app.use('/api', apiRouter(database));
   app.use('*', (req, res) => {
     return res.sendFile(path.join(_dirname, 'public/index.html'))
