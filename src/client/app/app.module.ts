1cc6785d220b06b220111ec95a8c4bead39d5abd
Moh Kemal <mohakemal9@gmail.com>
2017-08-16 17:43:20 -0400

    login page with authentication is set up

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -11,6 +11,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AddContactComponent } from './add-contact/add-contact.component';
 import { ApiService } from './shared/api.service';
 import { AuthService } from './shared/auth.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AuthService } from './shared/auth.service';
     MenuComponent,
     ContactListComponent,
     ContactComponent,
-    AddContactComponent
+    AddContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
