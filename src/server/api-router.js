53032dfc40576be2917182907385ecd2a41f172b
Moh Kemal <mohakemal9@gmail.com>
2017-08-16 08:53:47 -0400

    api router created

---
+++ src/server/api-router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+function apiRouter(database) {
+  // create the instance of the express router
+  const router = express.Router();
+
+  router.get('/contacts', (req, res) => {
+    const contactsCollection = database.collection('contacts');
+
+        contactsCollection.find({}).toArray((err, docs) => {
+          return res.json(docs)
+        });
+
+      });
+
+      router.post('/contacts', (req, res) => {
+        const user = req.body;
+
+        const contactsCollection = database.collection('contacts');
+
+        contactsCollection.insertOne(user, (err, r) => {
+          if (err) {
+            return res.status(500).json({ error: 'Error inserting new record.' })
+          }
+
+          const newRecord = r.ops[0];
+
+          return res.status(201).json(newRecord);
+        });
+      });
+
+      return router;
+    }
+
+    module.exports = apiRouter;
+  })
+}
--- src/server/index.js
+++ src/server/index.js
@@ -6,11 +6,12 @@ const path = require('path');
 const createExpressApp = require('./create-express-app');
 
 require('dotenv').config();
-
+// create a database connection
 MongoClient.connect(process.env.DB_CONN, (err, db) => {
   console.log('connected to mongodb...');
-
+// once we create a database connection
   createExpressApp(db)
+  // we tell the server to listen port 3000
   .listen(3000, () => {
     database = db;
     console.log('listening on port 3000...');
