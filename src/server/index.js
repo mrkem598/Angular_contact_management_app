b45a8c13faf86041e91c83be38a49c9d507c05ae
Moh Kemal <mohakemal9@gmail.com>
2017-08-16 08:16:59 -0400

    index.js code is refactored

--- src/server/index.js
+++ src/server/index.js
@@ -1,54 +1,18 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+const bodyParser = require('bodyParser');
 const path = require('path');
+const createExpressApp = require('./create-express-app');
 
 require('dotenv').config();
 
-let database;
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/profiles', express.static(path.join(__dirname, 'profiles')));
-app.use(bodyParser.json());
-
-app.get('/api/contacts', (req, res) => {
- 
-  const contactsCollection = database.collection('contacts');
-
-  contactsCollection.find({}).toArray((err, docs) => {
-    return res.json(docs)
-  });
-
-});
-
-app.post('/api/contacts', (req, res) => {
-  const user = req.body;
-
-  const contactsCollection = database.collection('contacts');
-
-  contactsCollection.insertOne(user, (err, r) => {
-    if (err) {
-      return res.status(500).json({ error: 'Error inserting new record.' })
-    }
-
-    const newRecord = r.ops[0];
-
-    return res.status(201).json(newRecord);
-  });
-});
-
-app.get('*', (req, res) => {
-  return res.sendFile(path.join(__dirname, 'public/index.html'))
-});
-
 MongoClient.connect(process.env.DB_CONN, (err, db) => {
-
   console.log('connected to mongodb...');
 
-  app.listen(3000, () => {
+  createExpressApp(db)
+  .listen(3000, () => {
     database = db;
     console.log('listening on port 3000...');
   });
-
 });
